5a704c2f5f34124bd3f57f4b567583159728d6e2
Microfed <microfed@gmail.com>
2015-11-04 04:39:40 +0300

    Add a component for bootstrap-like links, add redirects to first page

--- app/router.js
+++ app/router.js
@@ -3,6 +3,11 @@ import config from './config/environment';
 
 var Router = Ember.Router.extend({
   location: config.locationType
+
+  // waiting for https://github.com/thoov/ember-redirect/issues/14 to resolve
+  //redirects: {
+  //  'index': 'cars'
+  //}
 });
 
 Router.map(function() {
