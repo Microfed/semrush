474bf0be9f2c4e0a2a2dae0ad68c2018451d6ff1
Microfed <microfed@gmail.com>
2015-11-11 05:09:21 +0300

    Add image-preview component with simple tests suite
      - replace images in fixtures with different sizes;
      - add default placeholder for an image thumbnail;
      - organize styles a little.

---
+++ app/components/image-preview.js
@@ -0,0 +1,80 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  imageURI: '',
+
+  thumbURI: Ember.computed('imageURI', function () {
+    return this.get('imageURI');
+  }),
+
+  isThumbnailAvailable: Ember.computed('thumbURI', function () {
+    return !!this.get('thumbURI');
+  }),
+
+  _imageHTML: null,
+
+  showFullImage: function (imageHTML) {
+    var $img = this.$('img');
+
+    if ($img.length && imageHTML) {
+      $img.popover({ content: imageHTML, trigger: 'manual', html: true });
+
+      $img.popover('show');
+    }
+  },
+
+  loadFullImage: function () {
+    return new Promise((resolve, reject) => {
+      if (this.get('_imageHTML')) {
+        resolve(this.get('_imageHTML'));
+      } else {
+        let imageEl = new Image();
+
+        imageEl.onload = () => {
+          imageEl.onload = null;
+
+          let imageHTML = imageEl.outerHTML;
+          this.set('_imageHTML', imageHTML);
+
+          resolve(imageHTML);
+        };
+
+        imageEl.onerror = () => {
+          imageEl.onerror = null;
+
+          reject();
+        };
+
+        imageEl.src = this.get('imageURI');
+      }
+    });
+  },
+
+  setupPopover: function () {
+    var $img = this.$('img');
+
+    $img.hover(() => {
+      this.loadFullImage().then(imgHTML => this.showFullImage(imgHTML))
+    }, () => {
+      $img.popover('hide');
+    });
+  },
+
+  didRender: function () {
+    var $img = this.$('img');
+
+    if (this.get('isThumbnailAvailable')) {
+
+      $img.on('error', () => {
+        this.$('img').off('error');
+
+        Ember.run(this, function () {
+          this.set('isThumbnailAvailable', false);
+        });
+      });
+
+      this.setupPopover();
+    }
+  }
+});
