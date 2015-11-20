ddb4d8e9687387e9f50b89eb2c0b351f6a0a60c6
Microfed <microfed@gmail.com>
2015-11-20 16:40:24 +0300

    Fix test case

--- app/helpers/capitalize-string.js
+++ app/helpers/capitalize-string.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function capitalizeString(params/*, hash*/) {
-  let string = params[0] || '';
+  let string = (params[0] || '').toString(); // in case of numbers and so on
   let capitalized = string.charAt(0).toUpperCase() + string.substring(1);
 
   return Ember.String.htmlSafe(capitalized);
