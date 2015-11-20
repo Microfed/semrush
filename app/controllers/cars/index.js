7646a575b868d45fc3cc719a1d5bbd6d8cb8ae79
Microfed <microfed@gmail.com>
2015-11-20 17:35:59 +0300

    Add github pages

--- app/controllers/cars/index.js
+++
@@ -1,4 +0,0 @@
-import Ember from 'ember';
-import CarsListMixin from 'semrush/mixins/cars-list-mixin';
-
-export default Ember.Controller.extend(CarsListMixin);
--- app/models/brand-picking-records.js
+++
@@ -1,5 +0,0 @@
-import StorageArray from 'ember-local-storage/local/array';
-
-export default StorageArray.extend({
-  storageKey: 'semrush-brand-picking-records'
-});
